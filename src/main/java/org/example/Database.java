8b1a5ff2cd7faebfb880ba8f1ae17b203cf5a34f
Miguel R <miguel@debloat.us>
2022-12-09 12:47:02 -0500

    better display

--- src/main/java/org/example/Database.java
+++ src/main/java/org/example/Database.java
@@ -7,10 +7,6 @@ import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 
-import static org.example.Main.main;
-import static org.example.Main.menu2;
-
-
 public class Database {
 	public List<Employee> employees;
 
